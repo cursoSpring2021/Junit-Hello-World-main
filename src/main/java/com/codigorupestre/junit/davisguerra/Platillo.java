de6a98d946e68c07b83cab3f62e9f63c14431bee
cursoSpring2021 <dguerra723@unab.edu.co>
2022-03-14 17:17:27 -0500

    Prueba de un platillo

---
+++ src/main/java/com/codigorupestre/junit/davisguerra/Platillo.java
@@ -0,0 +1,14 @@
+package com.codigorupestre.junit.davisguerra;
+
+public class Platillo {
+	public String getTacos() {
+		return tacos;
+	}
+
+	public void setTacos(String tacos) {
+		this.tacos = tacos;
+	}
+
+	private String tacos;
+
+}
