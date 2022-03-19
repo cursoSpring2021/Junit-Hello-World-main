3566a7dffe47a11f06cde21e8ad5d12adb0e2368
cursoSpring2021 <dguerra723@unab.edu.co>
2022-03-19 14:37:44 -0500

    Modifición delm archivo de prueba

--- src/main/java/com/codigorupestre/junit/davisguerra/Platillo.java
+++ src/main/java/com/codigorupestre/junit/davisguerra/Platillo.java
@@ -1,14 +1,25 @@
 package com.codigorupestre.junit.davisguerra;
 
 public class Platillo {
+	private String tacos;
+	private String bandelaPaisa;
+	public String getBandelaPaisa() {
+		return bandelaPaisa;
+	}
+
+	public void setBandelaPaisa(String bandelaPaisa) {
+		this.bandelaPaisa = bandelaPaisa;
+	}
+
 	public String getTacos() {
 		return tacos;
+		
 	}
 
 	public void setTacos(String tacos) {
 		this.tacos = tacos;
 	}
 
-	private String tacos;
+	
 
 }
